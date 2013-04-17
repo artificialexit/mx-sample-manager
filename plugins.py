b71e2f8d1c143136822d3205bf9cfd7b0bbe186d
Nathan Mudie <nathan.mudie@gmail.com>
2013-04-17 14:27:41 +1000

    add decorator for protecting pages using beamline or vbl plugins

--- plugins.py
+++ plugins.py
@@ -2,8 +2,18 @@ from flask.ext.mongokit import MongoKit
 from flask.ext.pymongo import PyMongo
 from flask.ext.vbl import VBL
 from flask.ext.beamline import Beamline
+from functools import wraps
 
 db = MongoKit()
 mongo = PyMongo()
 vbl = VBL()
 beamline = Beamline()
+
+
+def beamline_or_vbl(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        if not (vbl.current_user or beamline.current):
+            return vbl.get_login_redirect()
+        return func(*args, **kwargs)
+    return decorated
